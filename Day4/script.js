06bcec0e48890e92726a773503f910954b358e3b
Abdullah Faisal <73385633+afaisal997@users.noreply.github.com>
2025-02-27 12:28:54 +0530

    Add files via upload

---
+++ Day4/script.js
@@ -0,0 +1,32 @@
+//Operators
+
+console.log(8+4) //12
+console.log("8"+4) //84
+console.log("8"+"4") //84
+console.log(8055+"Abdullah") //8055Abdullah
+
+let str = "8" + 4
+console.log(typeof(str)) //string
+
+
+console.log(8-4) //4
+console.log("8"-4) //4
+console.log("8"-"4") //4
+console.log("8"-"-4") //12
+console.log(8055-"Abdullah") //NaN
+
+
+console.log(8*4) //32
+console.log("8"*4) //32
+console.log("8"*"4") //32
+console.log(8055*"Abdullah") //NaN
+
+let b = 20;
+b+=30;
+console.log(b) //50
+
+let c = 20;
+c*=30;
+console.log(c) //600
+
+
