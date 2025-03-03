e688381eabf87cf71660996f0d94f1bf6dd3350e
Abdullah Faisal <73385633+afaisal997@users.noreply.github.com>
2025-03-03 19:36:12 +0530

    Add files via upload

---
+++ Day5/script.js
@@ -0,0 +1,61 @@
+// console.log(10<20) //true
+// console.log(10>20) //false
+// console.log(10<=20) //true
+// console.log(10>=20) //false
+
+// console.log(10==10) //true
+// console.log(10=="10") //true
+// console.log(10===20) //false
+// console.log(10==="10") //false
+// console.log("10"==="10") //true
+
+// console.log(10!=10) //false
+// console.log(10!=20) //true
+// console.log("10"!="10") //false
+
+// console.log(10!==10) //false
+// console.log(10!==20) //true
+// console.log("10"!=="10") //false
+
+// console.log(true && false); //false
+// console.log(10 && 20); //20
+// console.log(10 && 0); //0
+// console.log(0 && 10); //0
+// console.log(0 && 0); //0
+// console.log(false && 0); //false
+
+// console.log(true || false); //false
+// console.log(10 || 20); //20
+// console.log(10 || 0); //0
+// console.log(0 || 10); //0
+// console.log(0 || 0); //0
+// console.log(false || 0); //false
+
+// console.log(!10) //false
+// console.log(!true) //false
+// console.log(!false) //true
+// console.log(!0) //true
+
+// console.log(4&5) //4
+// console.log(4|5) //5
+// console.log(4^5) //1
+
+// let a=4
+// let b=2
+// console.log(a<<b) //16
+// let x=4
+// let y=1
+// console.log(x<<y) //8
+
+// let c=4
+// let d=2
+// console.log(c>>d) //1
+// let e=4
+// let f=1
+// console.log(e>>f) //2
+
+let num = 20;
+let n = -30;
+console.log(~num); // Output: -21
+console.log(~n); //29
+console.log(~10); //-11
