eaa24c612d476e25d27b9c3950f8e81110d08b0e
Abdullah Faisal <73385633+afaisal997@users.noreply.github.com>
2025-03-04 13:27:41 +0530

    Add files via upload

---
+++ Day6/script.js
@@ -0,0 +1,90 @@
+// var num=5;
+// num%2==0?console.log(`even number is ${num}`):console.log(`add number is ${num}`)
+
+
+
+
+// let x =10;
+// let y = x++;
+// console.log(x) //11
+// console.log(y) //10
+
+// let a = 10;
+// let b = ++a +20;
+// console.log(a) //11
+// console.log(b) //32
+
+// let a1 = 10;
+// let b1 = ++a1;
+// console.log(a1) //11
+// console.log(b1) //11
+
+// Post Increment
+
+// let x=10;
+// let y=x++;
+// console.log(x) //11
+// console.log(y) //10
+
+
+
+
+
+
+// IF STATEMENT 
+
+// if(true){
+//     console.log('Hello world1')
+//     console.log('Hello world2')
+//     console.log('Hello world3')
+//     console.log('Hello world4')
+// }
+
+// if(false)
+//     console.log('Hello world1')
+//     console.log('Hello world2')
+//     console.log('Hello world3')
+//     console.log('Hello world4')
+
+// var x ;
+// if(x){
+//     console.log('Hello world1')
+// }
+
+
+
+
+
+// Q. Write a program to check whether a number is even or odd using only if statement.
+// let a = 10;
+// if(a%2==0) console.log(`even number is ${a}`)
+
+// if(a%2!=0){
+//     console.log(`odd number is ${a}`)
+// }
+
+
+
+
+// var amount = 200;
+// if(amount>=200)
+// {
+//     console.log(`lets have coffee in CCD`)
+// }
+// else{
+//     console.log(`lets have tea`)
+// }
+
+
+
+// Q) WAP TO SUM OF TWO NUMBERS IF CONDITION TRUE 
+
+let a=10
+let b=20
+
+if(a<b){
+    console.log(a+b)
+}
+else{
+    console.log(a-b)
+}
---
+++ Day6/task1.js
@@ -0,0 +1,8 @@
+// WAP to check whether a person is eligible for voting or not
+
+let age = Number(prompt("Enter your age: "));
+if (age >= 18) {
+  alert(`You are eligible for voting`);
+} else {
+  alert(`You are not eligible for voting`);
+}
