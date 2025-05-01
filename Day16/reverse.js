55b275371f1bf55e713c9beb6a0cd7a3f3e65256
Abdullah Faisal <73385633+afaisal997@users.noreply.github.com>
2025-05-02 02:21:45 +0530

    Add files via upload

---
+++ Day16/reverse.js
@@ -0,0 +1,2 @@
+let arr = [11, 27, 33, 24, 55, 66];
+console.log(arr.reverse());
---
+++ Day16/sort.js
@@ -0,0 +1,6 @@
+const arr = ["Faisal", "Zakiya", "Abdullah", "Danish", "Hanzalah"];
+console.log(arr.sort());
+
+let arr1 = [11, 22, 5, 7, 9, 33, 24, 55, 66];
+arr1.sort((x, y) => x - y);
+console.log(arr1);
