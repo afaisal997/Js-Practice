4d9f9709e1e9198dc3da4d85839b50293e46e6be
Abdullah Faisal <73385633+afaisal997@users.noreply.github.com>
2025-05-02 01:32:23 +0530

    Add files via upload

---
+++ Day16/arrDistruct.js
@@ -0,0 +1,8 @@
+const data = [11, "Abdullah Faisal", "Engineer", "New Delhi"];
+let [id, name, dsg, address] = data;
+console.log(`
+    ID      : ${id}
+    Name    : ${name}
+    Dsg     : ${dsg}
+    Address : ${address}
+    `);
---
+++ Day16/filter.js
@@ -0,0 +1,5 @@
+let arr = [11, 27, 33, 24, 55, 66];
+let x = arr.filter(function (element) {
+  return element > 20;
+});
+console.log(x);
